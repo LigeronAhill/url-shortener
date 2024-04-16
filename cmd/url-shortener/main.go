e7a1f85f357012b5588a5dec6269909b8307f8d2
Aleksandr <provotorov@inbox.ru>
2024-04-16 22:48:48 +0300

    storage

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,3 +1,56 @@
 package main
 
-func main() {}
+import (
+	"log/slog"
+	"os"
+
+	"github.com/LigeronAhill/url-shortener/internal/config"
+	"github.com/LigeronAhill/url-shortener/internal/lib/logger/sl"
+	"github.com/LigeronAhill/url-shortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// init config
+	cfg := config.MustLoad("config/local.yaml")
+
+	// init logger
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	// init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	g, err := storage.GetURL("googled")
+	if err != nil {
+		log.Error("failed to get url", sl.Err(err))
+		os.Exit(1)
+	}
+	log.Info("Got url for 'google' alias:", slog.String("url", g))
+
+	// TODO: init router: chi, chi render
+
+	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad(configPath string) *Config {
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file '%s' doesn't exists", configPath)
+	}
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+	return &cfg
+}
